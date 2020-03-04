ad26a6991c1d3289652c92c5a50681827967a7fe
cschweda <cschweda@gmail.com>
2020-03-04 11:21:41 -0600

    fix: adjust styling

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -8,22 +8,36 @@
               :headers="headers"
               :items="items"
               :items-per-page="-1"
-              class="elevation-1 hover"
+              class="elevation-1 hover my-8"
               show-expand
               item-key="name"
               :single-expand="singleExpand"
               :expanded.sync="expanded"
               @click:row="clicked"
+              hide-default-footer
             >
+              <template v-slot:item.name="{ item }">
+                <strong>{{ item.name }}</strong>
+              </template>
+
+              <template v-slot:item.badgeID="{ item }">
+                <span v-html="generateNetlifyBadge(item)"></span>
+              </template>
+
+              <template v-slot:item.status="{ item }">
+                <v-chip class="ma-2" dark :color="getStatusColor(item.status)">
+                  <strong> {{ item.status }}</strong>
+                </v-chip>
+              </template>
               <template v-slot:expanded-item="{ headers, item }">
                 <td :colspan="headers.length + 2">
-                  <v-card class="py-2" color="grey lighten-3">
+                  <v-card class="py-2" color="grey lighten-4">
                     <div class="px-5 py-5">
                       <h2
                         style="margin: 0; padding: 5px; font-size: 14px; color: #222"
                         class="mb-5"
                       >
-                        Response:
+                        STATUS RESPONSE:
                       </h2>
                       <div style="12px;">{{ item }}</div>
                     </div>
@@ -34,7 +48,7 @@
           </div>
           <div v-else class="text-center mt-12">
             <div style="font-weight: 900; font-size: 14px;">
-              Checking status
+              Fetching status
             </div>
             <v-progress-circular
               indeterminate
@@ -72,10 +86,10 @@ export default {
           sortable: true,
           value: "name"
         },
-        { text: "Category", value: "category" },
-        { text: "Status", value: "status" },
-        { text: "Duration", value: "duration" },
-        { text: "", value: "badgeID" }
+
+        { text: "Status", value: "status", align: "center" },
+        { text: "Response", value: "duration", align: "center" },
+        { text: "Build Status", value: "badgeID", align: "center" }
       ]
     };
   },
@@ -107,6 +121,23 @@ export default {
     NProgress.done();
   },
   methods: {
+    getStatusColor(status) {
+      if (this.$myApp.config.greenStatus.includes(status)) {
+        return "green darken-2";
+      } else {
+        return "red darken-2";
+      }
+    },
+    generateNetlifyBadge(item) {
+      if (!item.badgeID) return;
+      return `  <img
+              src="https://camo.githubusercontent.com/2c5ec3a1c1131650d17ff965fcb971dfe7ecdd58/68747470733a2f2f6170692e6e65746c6966792e636f6d2f6170692f76312f6261646765732f63356262343932392d643430362d346366302d613832632d6538303363336561656233342f6465706c6f792d737461747573"
+              alt="Netlify Status"
+              data-canonical-src="https://api.netlify.com/api/v1/badges/${item.badgeID}/deploy-status"
+              style="max-width:100%;"
+            />`;
+    },
+
     clicked(value) {
       if (value === this.expanded[0]) {
         this.expanded = [];
@@ -114,24 +145,8 @@ export default {
         if (this.expanded.length) {
           this.expanded.shift();
           this.expanded.push(value);
-          //console.log("send preview event here: ", this.expanded[0].title);
-          if (this.expanded[0].title) {
-            // this.$ga.event({
-            //   eventCategory: "Resource",
-            //   eventAction: "Preview",
-            //   eventLabel: "Preview: " + this.expanded[0].title
-            // });
-          }
         } else {
           this.expanded.push(value);
-          //console.log("send preview event here: ", this.expanded[0].title);
-          if (this.expanded[0].title) {
-            // this.$ga.event({
-            //   eventCategory: "Resource",
-            //   eventAction: "Preview",
-            //   eventLabel: "Preview: " + this.expanded[0].title
-            // });
-          }
         }
       }
     }
